a21a97ec9ef30043f4dbff588bf747c23b518c44
Ricardo Perez <rxperez12@gmail.com>
2024-05-01 12:21:16 -0700

    added user model

--- blogly/models.py
+++ blogly/models.py
@@ -1,1 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+dbx = db.session.execute
+
 """Models for Blogly."""
+
+
+class User (db.Model):
+    """User"""
+
+    __tablename__ = 'users'
+
+    id = db.mapped_column(
+        db.Integer,
+        db.Identity(),
+        primary_key=True
+    )
+
+    first_name = db.mapped_column(
+        db.String(50),
+        nullable=False
+    )
+
+    last_name = db.mapped_column(
+        db.String(50),
+        nullable=False
+    )
+
+    image_url = db.mapped_column(
+        db.String(100)
+    )
+
+    def get_full_name(self):
+        """Returns first name and last name as a string spearated by a space
+        """
+        return f'{self.first_name} {self.last_name}'
