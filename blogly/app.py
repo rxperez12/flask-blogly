43b0881788775c677cfdb1c28f980dbd7b5e9bde
Ricardo Perez <rxperez12@gmail.com>
2024-05-01 12:29:07 -0700

    added route for homepage

--- blogly/app.py
+++ blogly/app.py
@@ -1,9 +1,26 @@
 """Blogly application."""
 
+from flask import Flask, request, redirect, render_template
 import os
+from models import db, dbx, User
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.exceptions import NotFound
 
-from flask import Flask
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_RECORD_QUERIES'] = True
+db.init_app(app)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "shhh-secret")
+
+
+debug = DebugToolbarExtension(app)
+
+
+@app.get('/')
+def homepage():
+
+    return render_template('base.jinja')
